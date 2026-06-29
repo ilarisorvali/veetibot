0e9ee5183ccb6d474d522a9d7bfadc3cbea67f87
Ilari Sorvali <42145485+ilarisorvali@users.noreply.github.com>
2026-06-29 22:33:06 +0300

    Remove unnecessary service file copy command

--- deploy_services.sh
+++ deploy_services.sh
@@ -9,5 +9,4 @@ export XDG_RUNTIME_DIR="/run/user/$(id -u)"
 cp veetibot.build /home/slack-veetibot/.config/containers/systemd/
 cp veetibot.container /home/slack-veetibot/.config/containers/systemd/
 systemctl --user daemon-reload
-cp /run/user/1005/systemd/generator/veetibot.service /home/slack-veetibot/.config/systemd/user
 systemctl --user enable veetibot
